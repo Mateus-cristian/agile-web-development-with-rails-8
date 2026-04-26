a8efed6510b7f442cc1bfdc963d2f8ff911b83d4
Mateus Cristian <mateus_cristian@outlook.com>
2026-04-26 14:20:43 -0300

    feat: create a cart and line_itens

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   has_one_attached :image
 
   after_commit -> { broadcast_refresh_later_to "products" }
@@ -16,5 +18,12 @@ class Product < ApplicationRecord
       return if acceptable_types.include?(image.content_type)
 
       errors.add(:image, "must be a GIF, JPG or PNG image")
+    end
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, "Line Items present")
+      throw :abort
+      end
     end
 end
